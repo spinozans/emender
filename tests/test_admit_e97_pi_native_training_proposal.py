72b06e5e24ba845690f0aecb2653ade70d19ff38
Erik Garrison <erik.garrison@gmail.com>
2026-09-16 02:07:24 +0000

    Admit authorized Pi-native training authority

---
+++ tests/test_admit_e97_pi_native_training_proposal.py
@@ -0,0 +1,9 @@
+from scripts.admit_e97_pi_native_training_proposal import PROPOSAL_SHA,STATEMENT,affine
+from scripts.plan_e97_pi_native_training_schedule import identity,permutation
+from types import SimpleNamespace
+
+def test_admission_affine_matches_runtime_planner_contract():
+ args=SimpleNamespace(authority_sha256='a'*64,pack_sha256='b'*64,sampler_key=975424,world_size=8,context_size=65536)
+ meta=identity(args)
+ assert affine(args.authority_sha256,args.pack_sha256,args.sampler_key,8,65536,0,438)==permutation(meta,'epoch-permutation',0,438)
+ assert len(PROPOSAL_SHA)==64 and STATEMENT=='I authorize the exact 32 update proposal.'
